ce4b9f6b6f27512bbeca9d26dcb12b4438be7ba1
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2025-01-21 17:02:48 -0500

    Create countpairsthatformacompletedayII.py

---
+++ countpairsthatformacompletedayII.py
@@ -0,0 +1,34 @@
+
+#3185
+#medium
+
+#Given an integer array hours representing times in hours, return an integer denoting the number of pairs i, j where i < j and hours[i] + hours[j] forms a complete day.
+
+#A complete day is defined as a time duration that is an exact multiple of 24 hours.
+
+#For example, 1 day is 24 hours, 2 days is 48 hours, 3 days is 72 hours, and so on.
+
+ 
+
+#Example 1:
+
+#Input: hours = [12,12,30,24,24]
+
+#Output: 2
+
+#Explanation: The pairs of indices that form a complete day are (0, 1) and (3, 4).
+
+
+#another stupid trick question that I couldn't solve - skip it:
+
+class Solution:
+    def countCompleteDayPairs(self, hours: List[int]) -> int:
+        res = 0
+        for i, h in enumerate(hours):
+            hours[i] = h % 24
+        print(hours)
+        bucket = [0] * 24
+        for i, h in enumerate(hours):
+            res += bucket[(24 - hours[i]) % 24]
+            bucket[h] += 1
+        return res
