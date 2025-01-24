9b266147554c9982870847b5cd17e7cded3087e7
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2025-01-23 22:21:18 -0500

    Update countunivaluesubtrees.py

--- countunivaluesubtrees.py
+++ countunivaluesubtrees.py
@@ -39,3 +39,38 @@ class Solution:
 
         f(root)
         return self.res
+
+
+#1/23/25 review:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countUnivalSubtrees(self, root: Optional[TreeNode]) -> int:
+        self.res = 0
+        def f(root):
+            if not root:
+                return True
+            r = f(root.right)
+            l = f(root.left)
+            if l and r:
+                if root and root.right:
+                    if root.val != root.right.val:
+                        return False
+
+
+                if root and root.left:
+                    if root.val != root.left.val:
+                        return False
+                self.res += 1
+                return True
+
+
+
+
+        f(root)
+        return self.res
