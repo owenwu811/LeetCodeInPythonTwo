867f8dd101cdb23b2fb0f4ad10d27219f34f215a
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2025-01-23 22:14:55 -0500

    Update diameterofn-arytree.py

--- diameterofn-arytree.py
+++ diameterofn-arytree.py
@@ -41,3 +41,42 @@ class Solution:
             return l1
         f(root)
         return self.res
+
+
+#1/23/25 refresher:
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children if children is not None else []
+"""
+
+class Solution:
+    def diameter(self, root: 'Node') -> int:
+        """
+        :type root: 'Node'
+        :rtype: int
+        """
+        self.res = 0
+        def f(root):
+            if not root.children:
+                return 0
+            first = 0
+            second = 0
+            for child in root.children:
+                maxdepth = 1 + f(child)
+                if maxdepth > first:
+                    prevfirst = first
+                    first = maxdepth
+                    second = prevfirst
+                elif maxdepth > second:
+                    second = maxdepth
+                self.res = max(self.res, first + second)
+            return first
+
+
+
+        f(root)
+        return self.res
