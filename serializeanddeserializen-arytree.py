dff27a8f37b43ab5799afcaaaf77d84bf5702e96
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2025-05-09 19:11:27 -0400

    Create serializeanddeserializen-arytree.py

---
+++ serializeanddeserializen-arytree.py
@@ -0,0 +1,44 @@
+
+#428
+#hard
+
+#Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+
+#Design an algorithm to serialize and deserialize an N-ary tree. An N-ary tree is a rooted tree in which each node has no more than N children. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that an N-ary tree can be serialized to a string and this string can be deserialized to the original tree structure.
+
+#For example, you may serialize the following 3-ary tree
+
+
+#my own solution using python3:
+
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        if children is None:
+            children = []
+        self.val = val
+        self.children = children
+"""
+
+class Codec:
+    def serialize(self, root: 'Node') -> str:
+        """Encodes a tree to a single string.
+        
+        :type root: Node
+        :rtype: str
+        """
+        return root
+	
+    def deserialize(self, data: str) -> 'Node':
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: Node
+        """
+        return data
+        
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
